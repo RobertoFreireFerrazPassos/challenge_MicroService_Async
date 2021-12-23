80038b9e3536f4a125a9b8efa2fffb01c33bad0b
Roberto Freire Ferraz Passos <roberto.passos@radixeng.com.br>
2021-12-23 12:23:10 -0300

    Added mock validation for credit card

--- appshop/CreditCardProcessor/Events/Consumers/AppPurchasedConsumer.cs
+++ appshop/CreditCardProcessor/Events/Consumers/AppPurchasedConsumer.cs
@@ -1,4 +1,5 @@
 ﻿using ApiAppShop.Domain.Events;
+using CreditCardProcessor.Services.Validation;
 using MassTransit;
 using System;
 using System.Threading.Tasks;
@@ -11,8 +12,19 @@ namespace ApiAppShop.Domain.Consumers
         {
             var userId = context.Message.UserId;
             var appId = context.Message.AppId;
+            var creditCard = context.Message.CreditCard;
+            bool validCreditCard = CreditCardValidator.Validate(creditCard);
+            string creditCardNumber = CreditCardValidator.GetCreditCardLast4Numbers(creditCard);
 
-            Console.WriteLine($"App: [{appId}] purchased by {userId}");
+            if (validCreditCard)
+            {
+                Console.WriteLine($"Valid Credit Card {creditCardNumber}");
+                Console.WriteLine($"App: [{appId}] purchased by {userId}");
+            }
+            else 
+            {
+                Console.WriteLine($"Credit Card {creditCardNumber} denied");
+            }   
         }
     }
 }
---
+++ appshop/CreditCardProcessor/Services/Validation/CreditCardValidator.cs
@@ -0,0 +1,24 @@
+﻿using ApiAppShop.Domain.Events;
+using System;
+
+namespace CreditCardProcessor.Services.Validation
+{
+    public class CreditCardValidator
+    {
+        public static bool Validate(CreditCard creditCard)
+        {
+            // validate credit card
+            Random rng = new Random();
+            return rng.Next(0, 2) > 0;
+        }
+
+        public static string GetCreditCardLast4Numbers(CreditCard creditCard)
+        {
+            string creditCardNumber = creditCard.Number; 
+            int  creditCardNumberLength = creditCardNumber.Length;
+            if (creditCardNumberLength <= 4) return "";
+
+            return "**" + creditCardNumber.Substring(creditCardNumberLength - 4);
+        }
+    }
+}
