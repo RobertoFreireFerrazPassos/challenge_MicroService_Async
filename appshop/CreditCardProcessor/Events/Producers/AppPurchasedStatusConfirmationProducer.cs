688b5bf50fede0fa3988d707612b4fc09ecd4fcf
Roberto Freire Ferraz Passos <roberto.passos@radixeng.com.br>
2021-12-24 11:57:21 -0300

    Added configuration for rabbitmq

--- appshop/CreditCardProcessor/Events/Producers/AppPurchasedStatusConfirmationProducer.cs
+++ appshop/CreditCardProcessor/Events/Producers/AppPurchasedStatusConfirmationProducer.cs
@@ -1,22 +1,39 @@
 ﻿using ApiAppShop.Domain.Events;
 using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace CreditCardProcessor.Events.Producers
 {
     public class AppPurchasedStatusConfirmationProducer
     {
+        public static IConfigurationRoot configuration;
         public static async Task Publish(AppPurchasedStatusConfirmationEvent appPurchasedStatusConfirmation)
         {
+            ServiceCollection serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
             var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
             {
-                cfg.Host("amqp://guest:guest@rabbitmq:5672");
+                cfg.Host(configuration.GetConnectionString("RabbitMq"));
             });
 
             Console.WriteLine("Publishing AppPurchasedStatusConfirmation...");
 
             await busControl.Publish<AppPurchasedStatusConfirmationEvent>(appPurchasedStatusConfirmation);
+        }
+
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            serviceCollection.AddSingleton<IConfigurationRoot>(configuration);
         }
     }
 }
--- appshop/CreditCardProcessor/Program.cs
+++ appshop/CreditCardProcessor/Program.cs
@@ -3,17 +3,25 @@
     using CreditCardProcessor.Events.Infrastructure;
     using MassTransit;
     using MassTransit.RabbitMqTransport;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
     using System;
+    using System.IO;
     using System.Threading;
     using System.Threading.Tasks;
 
     class Program
-    {                  
+    {
+        public static IConfigurationRoot configuration;
+
         public static async Task Main()
         {
+            ServiceCollection serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
             var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
             {
-                cfg.Host("amqp://guest:guest@rabbitmq:5672");
+                cfg.Host(configuration.GetConnectionString("RabbitMq"));
                 RegisterReceiveEndpoints(cfg);                
             });
 
@@ -32,6 +40,16 @@
             {
                 await busControl.StopAsync();
             }
+        }
+
+        private static void ConfigureServices(IServiceCollection serviceCollection)
+        {
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+                .AddJsonFile("appsettings.json", false)
+                .Build();
+
+            serviceCollection.AddSingleton<IConfigurationRoot>(configuration);
         }
 
         private static void RegisterReceiveEndpoints(IRabbitMqBusFactoryConfigurator configurator)
