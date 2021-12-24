22540230a6ef146a19324d3b467149b06345f2f8
Roberto Freire Ferraz Passos <roberto.passos@radixeng.com.br>
2021-12-24 18:37:12 -0300

    AppPurchasedStatusConfirmationConsumer is caching app sucessfully

--- appshop/ApiAppShop.Events/Consumers/AppPurchasedStatusConfirmationConsumer.cs
+++ appshop/ApiAppShop.Events/Consumers/AppPurchasedStatusConfirmationConsumer.cs
@@ -1,4 +1,6 @@
-﻿using ApiAppShop.Domain.Events;
+﻿using ApiAppShop.Domain.Dtos;
+using ApiAppShop.Domain.Events;
+using ApiAppShop.Domain.Services;
 using MassTransit;
 using System;
 using System.Threading.Tasks;
@@ -7,11 +9,32 @@ namespace ApiAppShop.Domain.Consumers
 {
     public class AppPurchasedStatusConfirmationConsumer : IConsumer<AppPurchasedStatusConfirmationEvent>
     {
+        private readonly IAppService _appService;
+
+        public AppPurchasedStatusConfirmationConsumer(IAppService appService) 
+        {
+            _appService = appService;
+        }
+
         public async Task Consume(ConsumeContext<AppPurchasedStatusConfirmationEvent> context)
         {
-            string message = context.Message.StatusConfirmation ?
-                $"Purchase made by user {context.Message.UserId} on app {context.Message.AppId}" :
-                $"Purchase denied";
+            string message;
+            if (context.Message.StatusConfirmation)
+            {
+                message = $"Purchase made by user {context.Message.UserId} on app {context.Message.AppId}";
+                var newApp = new AppCreationDto()
+                {
+                    UserId = context.Message.UserId,
+                    Name = "Teste 2",
+                    Price = 4
+                };
+
+                _appService.AddAppByUser(newApp);
+            }
+            else
+            {
+                message = $"Purchase denied";
+            }                
             
             Console.WriteLine(message);
         }
